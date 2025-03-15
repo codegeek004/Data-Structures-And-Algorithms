8de96987276ab31498514e247ad9ec27d735c4b3
codegeek004 <yashvaishnav1404@gmail.com>
2025-03-15 10:13:34 +0530

    added delete and iterator

--- Linked-List/singly_linked_list.py
+++ Linked-List/singly_linked_list.py
@@ -16,6 +16,8 @@
 
 # del keyword deletes the reference variable not the object.
 
+###########################################################################################
+
 # node : type of all objects in a linked list where we store the item and reference to the next.
 # |Start=0| --> |[item][next=2]| --> |[item][next=3]| --> |[item][next=None]|
 
@@ -30,6 +32,7 @@
 #     4. traverse
 
 
+###########################################################################################
 
 class Node:
     def __init__(self, item=None, next=None):
@@ -66,27 +69,86 @@ class SinglyLinkedList:
             temp = temp.next
         return None
 
-    def insert_after(self, temp, data):
+    def insert_after(self, temp, data): #using temp I am passing the reference of the node after which I have to insert the data
+        #first search the node after which you have to insert the new value using the search method
         if temp is not None:
-            n=Node()
+            n=Node(data, temp.next)
+            temp.next = n
 
+    def print_list(self):
+        temp = self.start
+        while temp is not None:
+            print(temp.item, end = ' ')
+            temp = temp.next
+
+    def delete_first(self):
+        if self.start is not None:
+            self.start = self.start.next
+
+    def delete_last(self):
+        if self.start is None:
+            pass
+        elif self.start.next is None:
+            self.start = None
+        else:
+            temp = self.start
+            while temp.next.next is not None:
+                temp = temp.next
+            temp.next=None
+
+    def delete_item(self, data):
+        if self.start is None:
+            pass
+        elif self.start.next is None:
+            if self.start.item == data:
+                self.start = None
+        else:
+            temp = self.start
+            if temp.item == data:
+                self.start = temp.next
+            else:
+                while temp.next is not None:
+                    if temp.next.item == data:
+                        temp.next=temp.next.next
+                        break
+                    temp = temp.next
+
+
+    def __iter__(self):
+        return sll_iterator(self.start)
 
 
 #Driver code
-
 mylist = SinglyLinkedList()
 
-#check if linked list is empty
-print(mylist.is_empty())
+mylist.insert_at_start(14)
+mylist.insert_at_last(20)
+mylist.insert_at_start(4)
+mylist.insert_after(mylist.search(14), 144)
+mylist.print_list()
+
+print()
+mylist.delete_item(144)
+mylist.print_list()
+print()
+
+
+###iterating through the ll
+class sll_iterator:
+    def __init__(self,start):
+        self.current = start
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if not self.current:
+            raise StopIteration
+        data = self.current.item
+        self.current = self.current.next
+        return data
 
 
 
-
-
-
-
-
-
-
+for x in mylist:
+    print(x, end=' ')
 
 
