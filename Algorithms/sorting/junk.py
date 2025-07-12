847c0b7a62e9ed6d916ed92ab2cda786603099e1
codegeek004 <yashvaishnav1404@gmail.com>
2025-07-12 22:39:33 +0530

    sorting error resolved:

--- Algorithms/sorting/junk.py
+++ Algorithms/sorting/junk.py
@@ -1,18 +1,37 @@
-def InsertionSort(arr):
-    for i in range(1,len(arr)):
-        insert_position = i
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        insert = i
         temp = arr[i]
-        print('temp', temp)
         for j in range(i-1, -1, -1):
             if arr[j] > temp:
                 arr[j+1] = arr[j]
-                print('right shifting', arr)
-                insert_position = j
+                insert = j
             else:
                 break
-        arr[insert_position] = temp
-        print('after insertion', arr, '\n')
+        arr[insert] = temp
     return arr
-arr = [4,8,3,7,1,19]
-print(arr)
-print(InsertionSort(arr))
+
+def selection_sort(arr):
+    for i in range(0, len(arr)-1):
+        minval = i
+        for j in range(i+1, len(arr)):
+            if arr[i] > arr[j]:
+                minval = j
+        arr[i], arr[minval] = arr[minval], arr[i]
+    return arr
+
+def bubble_sort(arr):
+    for i in range(0, len(arr)-1):
+        for j in range(i+1, len(arr)):
+            if arr[i] > arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+            else:
+                break
+    return arr
+
+
+arr = [1,9,12,7,3,98]
+
+print(insertion_sort(arr))
+print(selection_sort(arr))
+print(bubble_sort(arr))
